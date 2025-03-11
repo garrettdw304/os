7e1ef9cdb8b0f1f7b09b646d5fb237b4586cab1f
Garrettdw304 <garrettdw14497@outlook.com>
2025-03-11 02:14:50 -0400

    removed old .bat files

--- make_boot.bat
+++
@@ -1,3 +0,0 @@
-@echo OFF
-call mkdir bin/boot
-call vasm6502_oldstyle -L bin/boot/bootlf.txt -Lall -Fbin -dotdir -wdc02 -o bin/boot/bootrom.bin src/boot/boot.s
--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo OFF
-call make.bat
-start X:\CodeProjects\compdes\compdes_gui\bin\Debug\net7.0-windows\compdes_gui.exe -rom -realtime
-start plink.exe -serial COM95 -sercfg 9600,8,n,1,N
